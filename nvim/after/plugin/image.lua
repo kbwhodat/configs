685248000f82bd19b35880c34edece860249675f
kato-byantalo <kato.byantalo@ukg.com>
2024-03-09 20:26:37 -0500

    updating change to speed up vim startup time

--- nvim/after/plugin/image.lua
+++ nvim/after/plugin/image.lua
@@ -24,10 +24,10 @@ require("image").setup({
       clear_in_insert_mode = true,
       download_remote_images = true,
       only_render_image_at_cursor = true,
-      filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+      filetypes = { "markdown" }, -- markdown extensions (ie. quarto) can go here
     },
     neorg = {
-      enabled = true,
+      enabled = false,
       clear_in_insert_mode = true,
       download_remote_images = true,
       only_render_image_at_cursor = false,
