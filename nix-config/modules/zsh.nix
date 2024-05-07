94237afd52052dcd026d58e11e3098ed1f2b4bb3
kbwhodat <byantalok@yahoo.com>
2024-05-06 23:51:19 -0400

    update

--- nix-config/modules/zsh.nix
+++ nix-config/modules/zsh.nix
@@ -2,22 +2,10 @@
 
 {
 	programs.zsh = {
-		enable = true;
+		enable = false;
 		shellAliases = {
 			ll = "ls -l";
 		};
-
-		interactiveShellInit = ''
-			export EDITOR="nvim"
-			export SHELL="$(which zsh)"
-			export BROWSER="firefox"
-			export GOPATH="$(echo $HOME/go)"
-			export PATH="/usr/local/bin:/home/katob/go/bin:/home/katob/.cargo/bin:/home/katob/.local/share/nvim/mason/bin:/usr/local/lib:/usr/local/opt/binutils/bin:/usr/local/opt/inetutils/libexec/gnubin:/usr/local/opt/openssl@3/bin:/usr/local/opt/bzip2/bin:$PATH"
-			export DYLD_LIBRARY_PATH="/usr/local/lib/"
-			export PATH="/opt/cuda/bin:$PATH"
-			export LD_LIBRARY_PATH="/opt/cuda/lib64:$LD_LIBRARY_PATH"
-# More environment variables can be added here
-			'';
 
 		enableCompletion = true;
 		autosuggestion.enable = true;
