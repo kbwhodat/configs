886f4e996b8947f4cc58dbea70b0ce77655563bc
kbwhodat <byantalok@yahoo.com>
2024-06-06 01:01:37 +0000

    adding hardware for server

--- nix-config/nixos/server/configuration.nix
+++ nix-config/nixos/server/configuration.nix
@@ -19,10 +19,10 @@
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = false;
-  #boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/nvme0n1";
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  #boot.loader.grub.enable = true;
+  #boot.loader.grub.device = "/dev/nvme0n1";
 
 
   networking.hostName = "nixos-server"; # Define your hostname.
