c2c96e6c973092ab8f0be65c646acf8786d45fb4
kbwhodat <byantalok@yahoo.com>
2024-08-29 18:00:34 -0400

    resolve conflict

--- nix-config/os/nixos/hosts/main/configuration.nix
+++ nix-config/os/nixos/hosts/main/configuration.nix
@@ -20,4 +20,6 @@
   networking.wireless.enable = false;  # Enables wireless support via wpa_supplicant.
   networking.wireless.interfaces = [ "wlp0s20f3" ];
 
+  services.logind.lidSwitchExternalPower = "ignore";
+
 }
