07c60a1cae0e489a20653a26685a130a50fb2c65
kbwhodat <byantalok@yahoo.com>
2025-08-13 07:16:11 -0400

    tmux continuum

--- nix-config/common/shell/tmux.nix
+++ nix-config/common/shell/tmux.nix
@@ -12,89 +12,97 @@ let
   };
   inherit (pkgs.stdenv) isDarwin;
 in
-
 {
+  programs.tmux = {
+    enable = true;
+    terminal = "screen-256color";
+    historyLimit = 100000;
 
-	programs.tmux = {
-		enable = true;
-		terminal = "screen-256color";
-		historyLimit = 100000;
-		plugins = with pkgs;
-		[
-			tmuxPlugins.better-mouse-mode
+    plugins = with pkgs; [
+      tmuxPlugins.better-mouse-mode
       tmuxPlugins.fzf-tmux-url
       tmux-fzf-session-switch
       tmuxPlugins.sessionist
       tmuxPlugins.extrakto
       tmuxPlugins.resurrect
-			tmuxPlugins.yank
-		];
+      tmuxPlugins.yank
+      tmuxPlugins.continuum
+    ];
 
-		extraConfig = ''
+    extraConfig = ''
+      set -g set-clipboard on
+      setw -g mode-keys vi
+      set -sg escape-time 0
 
-				set -g set-clipboard on
-				setw -g mode-keys vi
-				set -sg escape-time 0
+      # Resurrect strategies (keep, these are fine)
+      set -g @resurrect-strategy-nvim 'session'
+      set -g @resurrect-strategy-vim  'session'
+      set -g @resurrect-capture-pane-contents 'on'
 
-        set -g @resurrect-strategy-nvim 'session'
-        set -g @resurrect-strategy-vim 'session'
+      set -g @continuum-boot 'on'
+      set -g @continuum-restore 'on'
+      set -g @continuum-save-interval '10'
 
-				# set -g default-command "/run/current-system/sw/bin/bash"
-				# set-option -g default-shell "/run/current-system/sw/bin/bash"
-				set -g default-command "/etc/profiles/per-user/katob/bin/zsh"
-				set-option -g default-shell "/etc/profiles/per-user/katob/bin/zsh"
+      # Shell
+      set -g default-command "/etc/profiles/per-user/katob/bin/zsh"
+      set -g default-shell   "/etc/profiles/per-user/katob/bin/zsh"
 
-				set-option -g mouse on
-				set -g @yank_selection_mouse 'clipboard'
-				set -g cursor-color white
-				set -g prefix ${ if isDarwin then "C-a" else "C-a" }
-				unbind C-b
-				bind C-a send-prefix
-				bind R source-file ~/.config/tmux/tmux.conf \; display "Config Reloaded!"
+      set -g mouse on
+      set -g @yank_selection_mouse 'clipboard'
+      set -g cursor-color white
 
-        set -g status on
-        set -g status-left '#(
-          CURRENT_SESSION=$(tmux display-message -p "#S");
-          tmux ls \
-            | cut -d " " -f1 \
-            | tr "\n" " " \
-            | tr ":" " " \
-            | sed "s/\b$CURRENT_SESSION\b/#[fg=yellow]&#[default]/"
-        )'
-        set -g status-right ""
+      # Prefix
+      set -g prefix ${ if isDarwin then "C-a" else "C-a" }
+      unbind C-b
+      bind C-a send-prefix
+      bind R source-file ~/.config/tmux/tmux.conf \; display "Config Reloaded!"
 
-				set-option -g status-style bg=default
-        #set -g status-left "#S "
-        set-option -g status-left-length 100
-        set -g status-right-length 0
-        #set -g status-right ""
-				set -g window-status-current-format ""
-				set -g window-status-format ""
+      # Your custom left side is fine
+      set -g status-left '#(
+        CURRENT_SESSION=$(tmux display-message -p "#S");
+        tmux ls \
+          | cut -d " " -f1 \
+          | tr "\n" " " \
+          | tr ":" " " \
+          | sed "s/\b$CURRENT_SESSION\b/#[fg=yellow]&#[default]/"
+      )'
 
-        set-option -g detach-on-destroy off
+      set -g status on
+      set -g status-left-length 100
+      set -g status-right-length 50
 
-        # Enable cursor blink
-        set-option -g terminal-overrides ',*:cnorm=\E[?12l\E[?25h'
-        set-option -ga terminal-overrides ',*:civis=\E[?25l'
-        set-option -ga terminal-overrides ',*:Tc'
+      set -g status-right "."
 
-				set -gq allow-passthrough on
-				set -g visual-activity off
+      set -g status-interval 15
 
-				bind-key -n C-h select-pane -L
-				bind-key -n C-j select-pane -D
-				bind-key -n C-k select-pane -U
-        bind-key -n C-l select-pane -R
+      set -g status-style bg=default
 
+      # If you truly want no window labels:
+      set -g window-status-current-format ""
+      set -g window-status-format ""
 
-				bind-key -T copy-mode-vi 'C-h' select-pane -L
-				bind-key -T copy-mode-vi 'C-j' select-pane -D
-				bind-key -T copy-mode-vi 'C-k' select-pane -U
-				bind-key -T copy-mode-vi 'C-l' select-pane -R
-				bind-key -T copy-mode-vi v send-keys -X begin-selection
-        bind-key -T copy-mode-vi MouseDragEnd1Pane send-keys -X copy-pipe "xclip -selection clipboard -i"
+      set -g detach-on-destroy off
 
-		'';
-	};
+      # Terminal overrides
+      set -ga terminal-overrides ',*:cnorm=\E[?12l\E[?25h'
+      set -ga terminal-overrides ',*:civis=\E[?25l'
+      set -ga terminal-overrides ',*:Tc'
 
+      set -gq allow-passthrough on
+      set -g visual-activity off
+
+      # Pane movement (normal + copy mode)
+      bind-key -n C-h select-pane -L
+      bind-key -n C-j select-pane -D
+      bind-key -n C-k select-pane -U
+      bind-key -n C-l select-pane -R
+
+      bind-key -T copy-mode-vi C-h select-pane -L
+      bind-key -T copy-mode-vi C-j select-pane -D
+      bind-key -T copy-mode-vi C-k select-pane -U
+      bind-key -T copy-mode-vi C-l select-pane -R
+      bind-key -T copy-mode-vi v send-keys -X begin-selection
+      bind-key -T copy-mode-vi MouseDragEnd1Pane send-keys -X copy-pipe "xclip -selection clipboard -i"
+    '';
+  };
 }
