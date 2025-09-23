ec25f4a56708af6e23b2bf835bd2926f02b1a140
kbwhodat <byantalok@yahoo.com>
2025-09-23 09:54:02 -0400

    config change on wezterm

--- nix-config/common/shell/wezterm.nix
+++ nix-config/common/shell/wezterm.nix
@@ -7,6 +7,8 @@ in
 
 	programs.wezterm = {
 		enable = true;
+    enableZshIntegration = false;
+    enableBashIntegration = false;
 		extraConfig = ''
 
       local wezterm = require 'wezterm'
@@ -19,35 +21,42 @@ in
       end)
 
       return {
-        -- front_end = "OpenGL",
+        front_end = "OpenGL",
+        freetype_load_target = 'Light',
+        freetype_render_target = 'HorizontalLcd',
+        cell_width = 0.9,
+
         enable_wayland = false,
         enable_kitty_graphics = true,
         cursor_blink_ease_in = 'Linear',
         cursor_blink_ease_out = 'Linear',
+        cursor_thickness = "1pt",
+        bold_brightens_ansi_colors = "BrightOnly",
+
         keys = {
           { key = 'LeftArrow', mods = 'OPT', action = act.SendString '\x1bb' },
           { key = 'RightArrow', mods = 'OPT', action = act.SendString '\x1bf' },
         },
         color_scheme = "Alabaster",
-        window_decorations = "NONE",
+        window_decorations = "RESIZE",
         hide_tab_bar_if_only_one_tab = true,
         enable_tab_bar = false,
-        font_size = 13.0,
+        font_size = ${if isDarwin then "14.0" else "13.0"},
         default_prog = { "/etc/profiles/per-user/katob/bin/zsh" },
-        font = wezterm.font("CommitMono Nerd Font Mono"),
+        font = wezterm.font("CommitMono Nerd Font Mono", { weight = ${if isDarwin then "Bold" else "Normal"} }),
         font_rules = {
           {
             intensity = "Bold",
             italic = false,
-            font = wezterm.font("CommitMono Nerd Font Mono", { weight = "Bold", stretch = "Normal", style = "Normal" }),
+            font = wezterm.font("CommitMono Nerd Font Mono", { weight = "Bold", style = "Normal" }),
           },
           {
             intensity = "Bold",
             italic = true,
-            font = wezterm.font("CommitMono Nerd Font Mono", { weight = "Bold", stretch = "Normal", style = "Italic" }),
+            font = wezterm.font("CommitMono Nerd Font Mono", { weight = "Bold", style = "Normal" }),
           },
         },
-        window_background_opacity = 3.0,
+        window_background_opacity = 4.0,
         window_padding = {
           left = 0,
           right = 0,
@@ -60,42 +69,6 @@ in
           cursor_fg = 'black',
           cursor_bg = 'white',
           compose_cursor = 'orange',
-          tab_bar = {
-            background = '#0b0022',
-            active_tab = {
-              bg_color = '#2b2042',
-              fg_color = '#c0c0c0',
-
-              intensity = 'Normal',
-
-              underline = 'None',
-              italic = false,
-
-              strikethrough = false,
-            },
-
-            inactive_tab = {
-              bg_color = '#1b1032',
-              fg_color = '#808080',
-            },
-
-            inactive_tab_hover = {
-              bg_color = '#3b3052',
-              fg_color = '#909090',
-              italic = true,
-            },
-
-            new_tab = {
-              bg_color = '#1b1032',
-              fg_color = '#808080',
-            },
-
-            new_tab_hover = {
-              bg_color = '#3b3052',
-              fg_color = '#909090',
-              italic = true,
-            },
-          },
         },
       }
 		'';
